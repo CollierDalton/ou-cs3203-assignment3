7a2acf7e808fad339795993170b877efcc8f3cf5
CollierDalton <collierjdalton@gmail.com>
2023-02-21 22:05:23 -0600

    Step 3 (User Input)
    Created the ability to take user input for the numbers for the array. Created an array that users a pointer and can be used by other functions.

--- ou-cs3203-assignment3.cpp
+++ ou-cs3203-assignment3.cpp
@@ -1,24 +1,24 @@
 #include<iostream>
 using namespace std;
 
-int sumList(int a, int b, int c, int d) {
-	int array[4] = { a, b, c, d };
+int sumList(int size, int* array) { //use pointer of array to grab array from main
+	//int* array = new int[size];
 
 	int sum = 0;
 
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < size; i++) {
 		sum = sum + array[i];
 	}
 
 	return sum;
 }
 
-int productList(int a, int b, int c, int d){
-    int array[4] = {a, b, c, d};
+int productList(int size, int* array){ //use pointer of array to grab array from main
+    //int* array = new int[size];
 
     int product = 1;
 
-    for(int i = 0; i < 4; i++){
+    for(int i = 0; i < size; i++){
         product = product * array[i];
     }
 
@@ -27,8 +27,19 @@ int productList(int a, int b, int c, int d){
 }
 
 int main() {
-	cout << sumList(1, 2, 3, 4) << "\n";
-    cout << productList(1, 2, 3, 4) << "\n";
+	int size = 3;
+    //int* array = new int[size];
+    //int num1, num2, num3;
+
+    int* array = new int[size]; //sets the arrays size equal to the user input
+
+    for(int i = 0; i < size; i++){
+        cout << "Enter number " << i + 1 << ": ";   
+        cin >> array[i];    //grabs the value entered by the user and sets it to array[i]
+    }
+    
+    cout << "\n\nThe sum of the array is: " << sumList(size, array) << "\n";
+    cout << "The product of the array is: " << productList(size, array) << "\n";
 
 	return 0;
 }
