6f7e92e3842869ce697f3e2731dca7ade23775c3
CollierDalton <collierjdalton@gmail.com>
2023-02-21 21:24:46 -0600

    Step 2 (Product)
    Added the product function to multiply and get the product of a list.

--- ou-cs3203-assignment3.cpp
+++ ou-cs3203-assignment3.cpp
@@ -4,17 +4,31 @@ using namespace std;
 int sumList(int a, int b, int c, int d) {
 	int array[4] = { a, b, c, d };
 
-	int result = 0;
+	int sum = 0;
 
 	for (int i = 0; i < 4; i++) {
-		result = result + array[i];
+		sum = sum + array[i];
 	}
 
-	return result;
+	return sum;
+}
+
+int productList(int a, int b, int c, int d){
+    int array[4] = {a, b, c, d};
+
+    int product = 1;
+
+    for(int i = 0; i < 4; i++){
+        product = product * array[i];
+    }
+
+    return product;
+
 }
 
 int main() {
 	cout << sumList(1, 2, 3, 4) << "\n";
+    cout << productList(1, 2, 3, 4) << "\n";
 
 	return 0;
 }
